4de7b812694ccadfbf43fd6a9673ef8973bebfcb
khvci <88493840+khvci@users.noreply.github.com>
2023-02-23 11:24:54 +0300

    add second way of crawling

--- project02console/Program.cs
+++ project02console/Program.cs
@@ -1,9 +1,6 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace project02console
 {
@@ -20,6 +17,8 @@ namespace project02console
         private static string sterlinTitleXPath = "//div[@class='dovizBar mBot20']//a[3]//span[1]//span[1]";
         private static string sterlinAlisXPath = "//div[@class='dovizBar mBot20']//a[3]//span[2]//span[2]";
         private static string sterlinSatisXPath = "//div[@class='dovizBar mBot20']//a[3]//span[3]//span[2]";
+
+        private static string dovizXPath = "//div[@class='dovizBar mBot20']//a";
         static void Main(string[] args)
         {
             List<BigPara> dovizler = new List<BigPara>();
@@ -27,43 +26,71 @@ namespace project02console
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load("https://bigpara.hurriyet.com.tr/doviz/");
 
-            var dolarTitle = doc.DocumentNode.SelectSingleNode(dolarTitleXPath).InnerText;
-            var dolarAlis = doc.DocumentNode.SelectSingleNode(dolarAlisXPath).InnerText;
-            var dolarSatis = doc.DocumentNode.SelectSingleNode(dolarSatisXPath).InnerText;
+            #region part1
+            //var dolarTitle = doc.DocumentNode.SelectSingleNode(dolarTitleXPath).InnerText;
+            //var dolarAlis = doc.DocumentNode.SelectSingleNode(dolarAlisXPath).InnerText;
+            //var dolarSatis = doc.DocumentNode.SelectSingleNode(dolarSatisXPath).InnerText;
 
-            var euroTitle = doc.DocumentNode.SelectSingleNode(euroTitleXPath).InnerText;
-            var euroAlis = doc.DocumentNode.SelectSingleNode(euroAlisXPath).InnerText;
-            var euroSatis = doc.DocumentNode.SelectSingleNode(euroSatisXPath).InnerText;
+            //var euroTitle = doc.DocumentNode.SelectSingleNode(euroTitleXPath).InnerText;
+            //var euroAlis = doc.DocumentNode.SelectSingleNode(euroAlisXPath).InnerText;
+            //var euroSatis = doc.DocumentNode.SelectSingleNode(euroSatisXPath).InnerText;
 
-            var sterlinTitle = doc.DocumentNode.SelectSingleNode(sterlinTitleXPath).InnerText;
-            var sterlinAlis = doc.DocumentNode.SelectSingleNode(sterlinAlisXPath).InnerText;
-            var sterlinSatis = doc.DocumentNode.SelectSingleNode(sterlinSatisXPath).InnerText;
+            //var sterlinTitle = doc.DocumentNode.SelectSingleNode(sterlinTitleXPath).InnerText;
+            //var sterlinAlis = doc.DocumentNode.SelectSingleNode(sterlinAlisXPath).InnerText;
+            //var sterlinSatis = doc.DocumentNode.SelectSingleNode(sterlinSatisXPath).InnerText;
 
-            dovizler.Add(new BigPara
+            //dovizler.Add(new BigPara
+            //{
+            //    AlisFiyat = dolarAlis,
+            //    SatisFiyat = dolarSatis,
+            //    DovizTur = dolarTitle,
+            //});
+
+            //dovizler.Add(new BigPara
+            //{
+            //    AlisFiyat = euroAlis,
+            //    SatisFiyat = euroSatis,
+            //    DovizTur = euroTitle,
+            //});
+
+            //dovizler.Add(new BigPara
+            //{
+            //    AlisFiyat = sterlinAlis,
+            //    SatisFiyat = sterlinSatis,
+            //    DovizTur = sterlinTitle,
+            //});
+
+            //foreach (var item in dovizler)
+            //{
+            //    Console.WriteLine($"Doviz Tur: {item.DovizTur}, Alis: {item.AlisFiyat}, Satis: {item.SatisFiyat}");
+            //}
+            #endregion
+
+            #region part2
+
+            var dovizTotal = doc.DocumentNode.SelectNodes(dovizXPath);
+
+            foreach (var doviz in dovizTotal)
             {
-                AlisFiyat = dolarAlis,
-                SatisFiyat = dolarSatis,
-                DovizTur = dolarTitle,
-            });
+                var dovizTitle = doviz.SelectSingleNode(".//span[1]//span[1]").InnerText;
+                var dovizAlis = doviz.SelectSingleNode(".//span[2]//span[2]").InnerText;
+                var dovizSatis = doviz.SelectSingleNode(".//span[3]//span[2]").InnerText;
 
-            dovizler.Add(new BigPara
-            {
-                AlisFiyat = euroAlis,
-                SatisFiyat = euroSatis,
-                DovizTur = euroTitle,
-            });
-
-            dovizler.Add(new BigPara
-            {
-                AlisFiyat = sterlinAlis,
-                SatisFiyat = sterlinSatis,
-                DovizTur = sterlinTitle,
-            });
+                dovizler.Add(new BigPara()
+                {
+                    AlisFiyat = dovizAlis,
+                    SatisFiyat = dovizSatis,
+                    DovizTur = dovizTitle
+                });
+            }
 
             foreach (var item in dovizler)
             {
                 Console.WriteLine($"Doviz Tur: {item.DovizTur}, Alis: {item.AlisFiyat}, Satis: {item.SatisFiyat}");
             }
+
+            #endregion
+
             Console.WriteLine("X!");
         }
     }
